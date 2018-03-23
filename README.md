804076c1dd311fc318caf4e12c72540b37f3a071
Salvador Moreno <salvadormoreno@ugr.es>
2018-03-23 14:10:22 +0100

    Histograms added. Readme updated.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # australian-python
-An analysis of the australian dataset with Python
+An analysis of the Statlog (Australian Credit Approval) dataset with Python.
+
+The dataset is available at the UCI Machine Learning Repository ( https://archive.ics.uci.edu/ml/datasets/statlog+(australian+credit+approval) ). It contains the following variables:
+
+- A1: 0,1 CATEGORICAL (formerly: a,b)
+- A2: continuous.
+- A3: continuous.
+- A4: 1,2,3 CATEGORICAL (formerly: p,g,gg)
+- A5: 1, 2,3,4,5, 6,7,8,9,10,11,12,13,14 CATEGORICAL (formerly: ff,d,i,k,j,aa,m,c,w, e, q, r,cc, x)
+- A6: 1, 2,3, 4,5,6,7,8,9 CATEGORICAL (formerly: ff,dd,j,bb,v,n,o,h,z)
+- A7: continuous.
+- A8: 1, 0 CATEGORICAL (formerly: t, f)
+- A9: 1, 0 CATEGORICAL (formerly: t, f)
+- A10: continuous.
+- A11: 1, 0 CATEGORICAL (formerly t, f)
+- A12: 1, 2, 3 CATEGORICAL (formerly: s, g, p)
+- A13: continuous.
+- A14: continuous.
+- A15: 1,2 class attribute (formerly: +,-) 
--- australian.py
+++ australian.py
@@ -9,7 +9,8 @@ import matplotlib.pyplot as plt
 sys.setrecursionlimit(10000) # Recursion limit for KDTree.
 
 # Australian dataset characteristics
-COLUMN_NAMES = ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9', 'A10','A11', 'A12', 'A13', 'A14', 'Y']
+COLUMN_NAMES = ['A01', 'A02', 'A03', 'A04', 'A05', 'A06', 'A07', 'A08', 'A09', 'A10','A11', 'A12', 'A13', 'A14', 'Y']
+
 
 # Classes 
 class ExploratoryAnalysis(object):
@@ -18,6 +19,7 @@ class ExploratoryAnalysis(object):
 	"""
 	def __init__(self):
 		self.data = None
+		self.grouped = None
 		self.regressors = None
 		self.labels = None
 
@@ -29,25 +31,53 @@ class ExploratoryAnalysis(object):
 		:param limit: number of rows of the file to read
 		"""
 		self.data = pd.read_csv(csv_file, nrows = limit, comment = comment, header = header, names = names)
+		self.grouped = self.data.groupby('Y')
 
-# Exploratory analysis
+# Exploratory analysis 
 australian = ExploratoryAnalysis()
 australian.load_csv_file('australian.dat')
 
 australian.data.columns # There are 15 columns, 14 variables and 1 value to classify
 australian.data.describe() # Y values seems balanced for classification purpose.
 pd.isna(australian.data).sum() # There are no missing values
+australian.data.corr() # A08, A09, A10 look like relevant variables
 
-# Category VS continuous variables
+# Category vs continuous variables
 for name in COLUMN_NAMES:
 	print(name + ' -> ' + str(len(pd.unique(australian.data[name]))))
-	# Categorical: A1, A4, A6, A8, A9, A11, A12, Y
-	# Not sure, but defined as continuous: A5, A10
-	# Continuous: A2, A3, A7, A13, A14
+	# Categorical: A01, A04, A06, A08, A09, A11, A12, Y
+	# Not sure, but defined as continuous: A05, A10
+	# Continuous: A02, A03, A07, A13, A14
 
-for cat_var in ['A1', 'A4', 'A6', 'A8', 'A9', 'A11', 'A12', 'Y']:
-	australian.data[cat_var] = australian.data[cat_var].astype('category')
+#for cat_var in ['A01', 'A04', 'A06', 'A08', 'A09', 'A11', 'A12', 'Y']:
+#	australian.data[cat_var] = australian.data[cat_var].astype('category')
+	# Does not seem very useful in pandas, so it remains commented.
 
-# Plots
+# Plots 
+# TODO: implement plots in a method for the class
+# 	Histogram for A02
+fig, ax = plt.subplots(1,1)
+for name, group in australian.grouped:
+	ax.hist(group.A02, alpha = 0.5, label = 'Y = ' + str(name), bins=20)
 
+ax.legend()
+ax.set_title('A02') 
+plt.show()
 
+# 	Histogram for all variables
+fig, ax_array = plt.subplots(4,4)
+[[ax1,ax2,ax3,ax4], [ax5,ax6,ax7,ax8], [ax9,ax10,ax11,ax12], [ax13,ax14,_,_]] = ax_array
+for i in np.arange(14):
+	var = COLUMN_NAMES[i]
+	ax = ax_array.flatten()[i]
+	for name, group in australian.grouped:
+		ax.hist(group[var], alpha = 0.5, label = 'Y = ' + str(name))
+
+fig.legend()
+plt.show()
+
+# Commentaries ----------------------------------------------------------------
+#TODO: mirar sns (SEABORN) para análisis multivariables. Libros:
+#- Pandas: Pandas Cookbook
+#- seaborn: Python for Data Analysis, 2nd Edition
+#TODO: implement KNN classification
