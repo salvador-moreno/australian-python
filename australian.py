3d424a023928f5ca95c0c1563fbf501a28123a17
Salvador Moreno <salvadormoreno@ugr.es>
2018-03-23 10:26:58 +0100

    Category and continous variables separated

--- australian.py
+++ australian.py
@@ -8,6 +8,9 @@ import matplotlib.pyplot as plt
 
 sys.setrecursionlimit(10000) # Recursion limit for KDTree.
 
+# Australian dataset characteristics
+COLUMN_NAMES = ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9', 'A10','A11', 'A12', 'A13', 'A14', 'Y']
+
 # Classes 
 class ExploratoryAnalysis(object):
 	"""
@@ -27,9 +30,24 @@ class ExploratoryAnalysis(object):
 		"""
 		self.data = pd.read_csv(csv_file, nrows = limit, comment = comment, header = header, names = names)
 
-# Australian dataset
-COLUMN_NAMES = ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9', 'A10','A11', 'A12', 'A13', 'A14', 'Y']	
+# Exploratory analysis
 australian = ExploratoryAnalysis()
 australian.load_csv_file('australian.dat')
-australian.data.columns #15 columns
+
+australian.data.columns # There are 15 columns, 14 variables and 1 value to classify
+australian.data.describe() # Y values seems balanced for classification purpose.
+pd.isna(australian.data).sum() # There are no missing values
+
+# Category VS continuous variables
+for name in COLUMN_NAMES:
+	print(name + ' -> ' + str(len(pd.unique(australian.data[name]))))
+	# Categorical: A1, A4, A6, A8, A9, A11, A12, Y
+	# Not sure, but defined as continuous: A5, A10
+	# Continuous: A2, A3, A7, A13, A14
+
+for cat_var in ['A1', 'A4', 'A6', 'A8', 'A9', 'A11', 'A12', 'Y']:
+	australian.data[cat_var] = australian.data[cat_var].astype('category')
+
+# Plots
+
 
